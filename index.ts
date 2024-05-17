3fa79b4b12afaed961159220c9e0a91b1a4d3666
rukhsarkrn <rukdoll1@gmail.com>
2024-05-17 19:04:28 +0500

    colour changes, version changes

--- index.ts
+++ index.ts
@@ -4,30 +4,28 @@
 import inquirer from "inquirer";
 import chalk from "chalk";
 
-console.log(chalk.bgGray("\\\ ROCK, PAPER AND SCISSOR GAME"));
+console.log(chalk.bgMagenta("\n***** ROCK, PAPER AND SCISSOR GAME *****\n"));
 
-let answer = await inquirer.prompt([
-       {
+let answer = await inquirer.prompt([{
          name:"Player1",
-         message:"Player1 Choose your weapon",
+         message:(chalk.bgBlackBright.white("\n Player1 Choose Your Weapon")),
          type:"list",
          choices:["Rock", "Paper", "Scissor"]
          },
         {
            name:"Player2",
-           message:"Player2 Choose your weapon",
+           message:(chalk.bgBlackBright.white("\n Player2 Choose your weapon")),
            type:"list",
            choices:["Rock", "Paper", "Scissor"]
-}
-]);
+}]);
 
 if(answer.Player1===answer.Player2)
-    {console.log(chalk.red("ITS A TIE"));}
+    {console.log(chalk.red("\n ITS A TIE"));}
 else if (answer.Player1==="Rock" && answer.Player2 === "Scissor")
-     {console.log(chalk.yellow("PLAYER 1 WIN THE MATCH"));}
+     {console.log(chalk.yellow("\n PLAYER 1 WIN THE MATCH"));}
 else if (answer.Player1==="Paper" && answer.Player2 === "Rock")
-        {console.log(chalk.yellow("PLAYER 1 WIN THE MATCH"));}
+        {console.log(chalk.yellow("\n PLAYER 1 WIN THE MATCH"));}
 else if (answer.Player1==="Scissor" && answer.Player2 === "Paper") 
-       {console.log(chalk.yellow("PLAYER 1 WIN THE MATCH"));}
+       {console.log(chalk.yellow("\n PLAYER 1 WIN THE MATCH"));}
  else
- {console.log(chalk.green("PLAYER2 WIN THE MATCH"));}     
+ {console.log(chalk.green("\n PLAYER2 WIN THE MATCH"));}     
